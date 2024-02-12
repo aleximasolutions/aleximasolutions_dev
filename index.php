5686ba0994fb1efef4b3a320032cacdc9139002b
damiresz <damirabd879@gmail.com>
2024-02-12 14:27:15 +0100

    update

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<?php 
-session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Cache-Control" content="no-cache">
+    <link rel="icon" type="image/x-icon" href="image/icons/favicon.svg">
     <link rel="stylesheet" href="css/main.css" />
     <script defer src="libs/gsap/gsap.min.js"></script>
     <script defer src="libs/gsap/ScrollTrigger.min.js"></script>
