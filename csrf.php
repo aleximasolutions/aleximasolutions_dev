022ee2819cf85b0df3603686c8878e3b64a6a908
damiresz <damirabd879@gmail.com>
2024-02-13 07:35:38 +0100

    update

--- csrf.php
+++ csrf.php
@@ -2,18 +2,12 @@
 
 function generateCSRFToken()
 {
-    // Používá funkci random_bytes() k vygenerování náhodné posloupnosti bytů
-    $csrfToken = bin2hex(random_bytes(32));
-    // CSRF token je uložen v session pro pozdější kontrolu
-    $_SESSION['csrf_token'] = $csrfToken;
-    // Vrátí se vygenerovaný CSRF token
-    return $csrfToken;
+    return $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
 
 function verifyCSRFToken($submittedToken)
 {
-    // Zkontroluje, zda v session existuje CSRF token a zda je odeslaný token platný
     return (!empty($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $submittedToken));
 }
 
